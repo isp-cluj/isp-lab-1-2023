95cd428f29c2fbe67be9dfd2dfa6ae948730e95e
automatica-cluj <mihai.hulea@aut.utcluj.ro>
2025-02-16 08:23:24 +0200

    SImplify project structure.

---
+++ src/main/java/utcluj/aut/MyFirstExample.java
@@ -0,0 +1,32 @@
+package utcluj.aut;
+
+import java.util.Scanner;
+
+public class MyFirstExample {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        //read from console and print
+        System.out.println("Enter two numbers: ");
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println("Sum is: " + add(a, b));
+        System.out.println("Difference is: " + subtract(a, b));
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+
+
+
+}
--- src/test/java/lab/template/ExampleTest.java
+++ src/test/java/lab/template/ExampleTest.java
@@ -9,7 +9,7 @@ import static org.junit.Assert.assertEquals;
  */
 public class ExampleTest {
 
-    @Test
+    @org.junit.jupiter.api.Test
     public void testAdd(){
         assertEquals("Should add 2 numbers", 30, Example.add(10,20));
     }
